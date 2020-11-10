d731fb010f9e8f5b5ca65ee14ff378158e4b6600
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-11-10 10:31:44 +0530

    ADDED:maximumInteger method and testcases for maximumInterger

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,2 +1,12 @@
 public class Maximum {
+    public Integer maximumInteger(Integer x,Integer y,Integer z) {
+        Integer maximum = x;
+        if(y.compareTo(maximum)>0){
+            maximum=y;
+        }
+        if(z.compareTo(maximum)>0){
+            maximum=z;
+        }
+        return maximum;
+    }
 }
